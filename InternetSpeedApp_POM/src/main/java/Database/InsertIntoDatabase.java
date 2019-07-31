8759204b9721aa97fd40c0d717b5eeb7c65158ec
siyar-mayaya <siyar.mayaya@gmail.com>
2019-07-31 08:36:18 +0200

    - Write to MySQL Database

--- InternetSpeedApp_POM/src/main/java/Database/InsertIntoDatabase.java
+++ InternetSpeedApp_POM/src/main/java/Database/InsertIntoDatabase.java
@@ -10,9 +10,10 @@ import static ProjectSetup.BrowserFactory.driver;
 
 public class InsertIntoDatabase
 {
+    String empty = null;
     ResultSet rs = null;
     int Id;
-    String sqlStmt = "insert into speedtest.speedresults (Id, ping, download, upload) VALUES (?, ?, ?, ?)";
+    String sqlStmt = "insert into speedtest.speedresults (ping, download, upload) VALUES (?, ?, ?)";
 
     public void InsertIntoDatabase()
     {
@@ -21,34 +22,15 @@ public class InsertIntoDatabase
         String ping = speed.readPingValue();
         String download = speed.readDownloadSpeed();
         String upload = speed.readUploadSpeed();
-/*
-        String ping = "29";
-        String download = "19.65";
-        String upload = "15.20";
-*/
+
         try
         {
             Connection con = JConection.ConnectToDb();
             PreparedStatement prep = con.prepareStatement(sqlStmt);
-                try
-                {
-                    Statement smt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-                    rs = smt.executeQuery("SELECT Id FROM speedtest.speedresults");
-                }
-                catch(SQLException e)
-                {
-                    e.printStackTrace();
-                }
 
-
-            rs.last();
-            Id = rs.getInt("Id");
-            Id++;
-            String ID = Integer.toString(Id);
-            prep.setString(1, ID);
-            prep.setString(2, ping);
-            prep.setString(3, download);
-            prep.setString(4, upload);
+            prep.setString(1, ping);
+            prep.setString(2, download);
+            prep.setString(3, upload);
 
             prep.execute();
         }
